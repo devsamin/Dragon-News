e720c1b61ac823164b11917dd278ddffcc459ac9
devsamin <abdullahalsamin79@gmail.com>
2025-09-02 08:08:17 -0700

    Desgin left nav and show all categories

--- src/Layouts/HomeLayout.jsx
+++ src/Layouts/HomeLayout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from '../components/Header';
 import LatestNews from '../components/LatestNews';
 import Navbar from '../components/Navbar';
+import LeftNav from '../components/Layout-components/LeftNav';
 
 const HomeLayout = () => {
     return (
@@ -16,7 +17,9 @@ const HomeLayout = () => {
                 <Navbar></Navbar>
             </nav>
             <main className='w-11/12 mx-auto pt-5 grid grid-cols-12 gap-2'>
-                <aside className='col-span-3'>Left Nav</aside>
+                <aside className='col-span-3'>
+                    <LeftNav></LeftNav>
+                </aside>
                 <section className='col-span-6'>Main Nav</section>
                 <aside className='col-span-3'>Right Nav</aside>
             </main>
